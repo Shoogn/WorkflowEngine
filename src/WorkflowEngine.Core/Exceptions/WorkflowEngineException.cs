11ee7aef8bef0d005c7e343c5bb9668cad982b58
Mohammed Ahmed Hussien <Mohammed.Hussien@outlook.sa>
2022-11-18 12:35:00 +0300

    Add project files.

---
+++ src/WorkflowEngine.Core/Exceptions/WorkflowEngineException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkflowEngine.Core.Exceptions
+{
+    public class WorkflowEngineException : Exception
+    {
+        public WorkflowEngineException(string message) : base(message)
+        {
+
+        }
+    }
+}
---
+++ src/WorkflowEngine.Core/Models/Workflow.cs
@@ -0,0 +1,30 @@
+﻿namespace WorkflowEngine.Core.Models
+{
+    /// <summary>
+    /// Workflow object
+    /// </summary>
+    public sealed class Workflow
+    {
+        /// <summary>
+        /// Identitifire
+        /// </summary>
+        public int WorkflowId { get; set; }
+
+        /// <summary>
+        /// name in arabic
+        /// </summary>
+        public string NameAr { get; set; }
+
+        /// <summary>
+        /// name in english
+        /// </summary>
+        public string NameEn { get; set; }
+
+        /// <summary>
+        /// is active or not
+        /// by default is true
+        /// if is not active (false) the workflow can not procceed any flow
+        /// </summary>
+        public bool IsActive { get; set; } = true;
+    }
+}
